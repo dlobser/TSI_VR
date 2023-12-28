9e4163fb86276319f193ae41d3140421d4e45786
David Lobser <davidlobser@yahoo.com>
2023-12-28 15:45:46 -0500

    added some stuff

---
+++ Assets/Assignments/Assignment_AU 3/AnimatedVariables.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.VisualScripting;
+
+public class AnimatedVariables : MonoBehaviour
+{
+    public float overallFade = 1;
+
+    public float audioFade = 1;
+    
+    public float audioFadeA = 1;
+    public float audioFadeB = 1;
+    public float audioFadeC = 1;
+
+    public float visualFade = 1;
+
+    public float visualFadeA = 1;
+    public float visualFadeB = 1;
+    public float visualFadeC = 1;
+
+    public float overallSpeed = 1;
+
+    public float speedA = 1;
+    public float speedB = 1;
+    public float speedC = 1;
+
+    public float floatA;
+    public float floatB;
+    public float floatC;
+    public float floatD;
+
+    public Vector3 vectorA;
+    public Vector3 vectorB;
+    public Vector3 vectorC;
+    public Vector3 vectorD;
+
+    public Color colorA;
+    public Color colorB;
+    public Color colorC;
+    public Color colorD;
+}
